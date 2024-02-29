34a92965b4ea48465c0ac841a1941c9dc7baadc7
vbarindelli <virginiabarindelli@gmail.com>
2024-02-29 22:24:19 +0100

    conditionals code core and stretch

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,62 @@
 using System;
+using System.Runtime.CompilerServices;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        string userGrade = Console.ReadLine();
+        int grade = int.Parse(userGrade);
+        string letter = ("");
+        int lastDigit = grade % 10;
+        string sign = ("");
+
+        if (grade >= 90)
+        {
+            letter = "A";
+        }
+        else if (grade >= 80) 
+        {
+            letter = "B";
+        }
+        else if (grade >= 70)
+        {
+            letter = "C";
+
+        }
+        else if (grade >= 60)
+        {
+            letter = "D";
+        }
+        else 
+        {
+            letter = "F";
+        }
+
+        if (grade < 90 && lastDigit >= 7)
+        {
+            sign = "+";
+        }
+        else if (grade > 60 && lastDigit < 3)
+        {
+            sign = "-";
+        }
+        else
+        {
+            sign = "";
+        }
+
+        Console.WriteLine($"Your grade is {letter}{sign}.");
+
+        if (grade >= 70)
+        {
+            Console.WriteLine("Congratulations, you passed the class!");
+        }
+        else
+        {
+            Console.WriteLine("You did not pass the class. Keep up the hard work and you'll make it next time.");
+        }
+
     }
 }
