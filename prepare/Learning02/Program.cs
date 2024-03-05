e7ec5f0edf404f2db874ae1f83dd6a8dead4b264
vbarindelli <virginiabarindelli@gmail.com>
2024-03-05 11:01:44 +0100

    Week 2 Learning Activity code

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,26 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+       Job job1 = new Job();
+       job1._company = "Wildhorn";
+       job1._endYear = 2024;
+       job1._jobTitle = "Data Analyst";
+       job1._startYear = 2019;
+       
+
+       Job job2 = new Job();
+       job2._company = "SouthWorks";
+       job2._endYear = 2022;
+       job2._jobTitle = "Architect";
+       job2._startYear = 2000;
+      
+
+       Resume resume1 = new Resume();
+       resume1._name = "Virginia Haedo";
+       resume1._jobs.Add(job1);
+       resume1._jobs.Add(job2);
+      
+      resume1.DisplayResumeDetails();
+      
     }
 }
